7d934896a7ac7627dd08747be73a2f6a46ae0f83
nikkolistio <nikkolistiow@gmail.com>
2022-06-26 23:06:29 +0700

    edit contact.js header.js app.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div>
+      <img src={props.data.photo} alt={props.data.name} />
+      <div>
+        <h1>{props.data.name}</h1>
+        <h4>{props.data.phone}</h4>
+        <h4>{props.data.email}</h4>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
